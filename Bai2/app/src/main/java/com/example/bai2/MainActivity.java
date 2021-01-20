a0555e203ad31acc35a510843c99533aa3be00c0
DESKTOP-2PSSDB7\Huynh Huong <quyhaphong28@gmail.com>
2021-01-21 01:03:19 +0700

    Add Update Bai2

--- Bai2/app/src/main/java/com/example/bai2/MainActivity.java
+++ Bai2/app/src/main/java/com/example/bai2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bai2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.btn_XinChao);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Xin chào!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
